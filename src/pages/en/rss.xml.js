bed2287b4453f6b5add2e176cb010518dfd8b965
marceloduartebr <marcelo@duarte.top>
2026-09-21 15:42:58 -0300

    i18n: ES/EN RSS and locale path on static pages

---
+++ src/pages/en/rss.xml.js
@@ -0,0 +1,25 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { t } from '../../i18n/ui';
+
+const SITE = 'https://dsb.duarte.top';
+const locale = 'en';
+const copy = t(locale);
+
+export async function GET() {
+  const posts = (await getCollection('briefings')).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
+  return rss({
+    title: copy.rssTitle,
+    description: copy.rssDesc,
+    site: SITE,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/en/briefings/${post.id}`,
+      customData: `<enclosure url="${post.data.cover}" type="image/jpeg" />`,
+    })),
+  });
+}
